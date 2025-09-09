7d97131b99de8d0cba0699855978c685af04537c
ClaytonDewey <clay@dryadmedia.com>
2025-09-09 16:20:32 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  // https://open-meteo.com/
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      Units
+      <br />
+      Switch to Imperial/Metric
+      <br />
+      Temperature
+      <br />
+      Celsius (°C)
+      <br />
+      Fahrenheit (°F)
+      <br />
+      Wind Speed
+      <br />
+      km/h
+      <br />
+      mph
+      <br />
+      Precipitation
+      <br />
+      Millimeters (mm)
+      <br />
+      Inches (in)
+      <br />
+      How's the sky looking today?
+      <br />
+      Search for a city, e.g., New York
+      <br />
+      Search
+      <br />
+      Feels like
+      <br />
+      {/* <!-- Insert temperature here --> */}
+      Humidity
+      <br />
+      {/* <!-- Insert humidity here --> */}
+      Wind
+      <br />
+      {/* <!-- Insert wind here -->    */}
+      Precipitation
+      <br />
+      {/* <!-- Insert precipitation here --> */}
+      Daily forecast
+      <br />
+      {/* <!-- Insert daily forecast for the next 7 days here --> */}
+      Hourly forecast
+      <br />
+      {/* <!-- Insert hourly forecast for the selected day here --> */}
+      <footer class='attribution'>
+        Challenge by{' '}
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          href='https://www.frontendmentor.io?ref=challenge'
+          target='_blank'
+          rel='noreferrer'>
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by{' '}
+        <a href='https://www.claytondewey.com' target='_blank' rel='noreferrer'>
+          Clayton Dewey
+        </a>
+        .
+      </footer>
     </div>
   );
 }
