c17e7b0b0a51980024abb62b2d49d912f332f8d1
ClaytonDewey <clay@dryadmedia.com>
2025-10-07 12:47:14 -0500

    refactor: Update header & buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,111 +8,117 @@ export const Header = ({ onClick, units, theme, themeText, switchTheme }) => {
         <Icon name='logo' />
         <span className='sr-only'>Weather Now</span>
       </div>
-      <Toggler theme={theme} themeText={themeText} switchTheme={switchTheme} />
-      <div className='header__units'>
-        <Dropdown
-          buttonText={
-            <>
-              <Icon name='units' />
-              Units
-            </>
-          }
-          content={({ closeDropdown }) => (
-            <>
-              <Button
-                onClick={() => {
-                  closeDropdown();
-                  onClick();
-                }}
-                type='button'
-                className='btn btn-toggle'>
-                Switch to{' '}
-                <span>{units === 'imperial' ? 'metric' : 'imperial'}</span>
-              </Button>
-              <section className='unit'>
-                <h3>temperature</h3>
-                <Button
-                  type='button'
-                  className={`unit-item ${
-                    units === 'metric' ? 'active' : null
-                  }`}
-                  onClick={() => {
-                    closeDropdown();
-                    onClick();
-                  }}>
-                  Celcius(&deg;C)
-                  {units === 'metric' && <Icon name='check' />}
-                </Button>
-                <Button
-                  type='button'
-                  className={`unit-item ${
-                    units === 'imperial' ? 'active' : null
-                  }`}
-                  onClick={() => {
-                    closeDropdown();
-                    onClick();
-                  }}>
-                  Fahrenheit(&deg;F)
-                  {units === 'imperial' && <Icon name='check' />}
-                </Button>
-              </section>
-              <section className='unit'>
-                <h3>wind speed</h3>
-                <Button
-                  type='button'
-                  className={`unit-item ${
-                    units === 'metric' ? 'active' : null
-                  }`}
-                  onClick={() => {
-                    closeDropdown();
-                    onClick();
-                  }}>
-                  km/h
-                  {units === 'metric' && <Icon name='check' />}
-                </Button>
-                <Button
-                  type='button'
-                  className={`unit-item ${
-                    units === 'imperial' ? 'active' : null
-                  }`}
-                  onClick={() => {
-                    closeDropdown();
-                    onClick();
-                  }}>
-                  mph
-                  {units === 'imperial' && <Icon name='check' />}
-                </Button>
-              </section>
-              <section className='unit'>
-                <h3>precipitation</h3>
-                <Button
-                  type='button'
-                  className={`unit-item ${
-                    units === 'metric' ? 'active' : null
-                  }`}
-                  onClick={() => {
-                    closeDropdown();
-                    onClick();
-                  }}>
-                  Millimeters(mm)
-                  {units === 'metric' && <Icon name='check' />}
-                </Button>
-                <Button
-                  type='button'
-                  className={`unit-item ${
-                    units === 'imperial' ? 'active' : null
-                  }`}
-                  onClick={() => {
-                    closeDropdown();
-                    onClick();
-                  }}>
-                  Inches(in)
-                  {units === 'imperial' && <Icon name='check' />}
-                </Button>
-              </section>
-            </>
-          )}
+      <div className='header__buttons'>
+        <Toggler
+          theme={theme}
+          themeText={themeText}
+          switchTheme={switchTheme}
         />
+        <div className='header__units'>
+          <Dropdown
+            buttonText={
+              <>
+                <Icon name='units' />
+                Units
+              </>
+            }
+            content={({ closeDropdown }) => (
+              <>
+                <Button
+                  onClick={() => {
+                    closeDropdown();
+                    onClick();
+                  }}
+                  type='button'
+                  className='btn btn-toggle'>
+                  Switch to{' '}
+                  <span>{units === 'imperial' ? 'metric' : 'imperial'}</span>
+                </Button>
+                <section className='unit'>
+                  <h3>temperature</h3>
+                  <Button
+                    type='button'
+                    className={`unit-item ${
+                      units === 'metric' ? 'active' : null
+                    }`}
+                    onClick={() => {
+                      closeDropdown();
+                      onClick();
+                    }}>
+                    Celcius(&deg;C)
+                    {units === 'metric' && <Icon name='check' />}
+                  </Button>
+                  <Button
+                    type='button'
+                    className={`unit-item ${
+                      units === 'imperial' ? 'active' : null
+                    }`}
+                    onClick={() => {
+                      closeDropdown();
+                      onClick();
+                    }}>
+                    Fahrenheit(&deg;F)
+                    {units === 'imperial' && <Icon name='check' />}
+                  </Button>
+                </section>
+                <section className='unit'>
+                  <h3>wind speed</h3>
+                  <Button
+                    type='button'
+                    className={`unit-item ${
+                      units === 'metric' ? 'active' : null
+                    }`}
+                    onClick={() => {
+                      closeDropdown();
+                      onClick();
+                    }}>
+                    km/h
+                    {units === 'metric' && <Icon name='check' />}
+                  </Button>
+                  <Button
+                    type='button'
+                    className={`unit-item ${
+                      units === 'imperial' ? 'active' : null
+                    }`}
+                    onClick={() => {
+                      closeDropdown();
+                      onClick();
+                    }}>
+                    mph
+                    {units === 'imperial' && <Icon name='check' />}
+                  </Button>
+                </section>
+                <section className='unit'>
+                  <h3>precipitation</h3>
+                  <Button
+                    type='button'
+                    className={`unit-item ${
+                      units === 'metric' ? 'active' : null
+                    }`}
+                    onClick={() => {
+                      closeDropdown();
+                      onClick();
+                    }}>
+                    Millimeters(mm)
+                    {units === 'metric' && <Icon name='check' />}
+                  </Button>
+                  <Button
+                    type='button'
+                    className={`unit-item ${
+                      units === 'imperial' ? 'active' : null
+                    }`}
+                    onClick={() => {
+                      closeDropdown();
+                      onClick();
+                    }}>
+                    Inches(in)
+                    {units === 'imperial' && <Icon name='check' />}
+                  </Button>
+                </section>
+              </>
+            )}
+          />
+        </div>
       </div>
     </header>
   );
--- src/components/Toggler.jsx
+++ src/components/Toggler.jsx
@@ -1,21 +1,16 @@
-import { Input } from '../components/';
+import { Button } from '../components/';
 import { Icon } from '../svg';
 
 export const Toggler = ({ theme, themeText, switchTheme }) => {
   return (
     <div className='toggler'>
-      <div className='toggle-container'>
-        <label htmlFor='mode' className='label'>
-          <span>{themeText}</span>
-          {theme === 'light' ? <Icon name='moon' /> : <Icon name='sun' />}
-          <Input
-            type='checkbox'
-            id='mode'
-            className={`toggle ${theme}`}
-            onChange={switchTheme}
-          />
-        </label>
-      </div>
+      <Button
+        type='button'
+        className={`btn btn-secondary toggle ${theme}`}
+        onClick={switchTheme}>
+        <span>{themeText}</span>
+        {theme === 'light' ? <Icon name='moon' /> : <Icon name='sun' />}
+      </Button>
     </div>
   );
 };
